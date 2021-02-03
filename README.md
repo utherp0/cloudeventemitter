e2a405217655e80fb6347a385d2091a59927c4c1
utherp0 <ian.lawson@redhat.com>
2021-02-03 13:28:04 +0000

    First drop

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# cloudeventemitter
+Cloud Event Emitter
+
+This is a simple node.js app that can be deployed to OpenShift or Kubernetes and used to manual send cloud events to any knative broker reachable via the:
+
+http://broker-ingress.knative-eventing.svc.cluster.local/{namesapce}/default
+
---
+++ server.js
@@ -0,0 +1,76 @@
+//  OpenShift sample Node application
+var express = require('express');
+var fs      = require('fs');
+var app     = express();
+var eps     = require('ejs');
+var got     = require('got');
+
+var axios   = require('axios').default;
+const { HTTP, CloudEvent } = require("cloudevents");
+
+app.engine('html', require('ejs').renderFile);
+
+app.use( '/scripts', express.static('scripts'));
+app.use( '/styles', express.static('styles'));
+app.use( '/images', express.static('images'));
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+// Emit an event
+app.get('/emit', function (req,res)
+{
+  // Extract the parameters from the request
+  var broker = req.query.broker;
+  var cetype = req.query.cetype;
+  var payload = req.query.payload;
+
+  const cloudevent = new CloudEvent({
+    type: cetype,
+    data: {
+      payload
+    },
+    source: 'cloudeventemitter'
+  });
+
+  const message = HTTP.binary(cloudevent);
+
+  axios({
+     method: "post",
+     url: broker,
+     data: message.body,
+     headers: message.headers,
+  });
+
+  console.log( "Emitted cloud event " + cloudevent.type );
+
+  res.sendStatus(200);
+});
+
+// Serve the input page
+app.get('/', function (req, res)
+{
+  console.log( "Serving request to " + req.ip);
+ 
+  res.render('emitter.html');
+});
+
+// Return the default broker ENV if present
+app.get('/broker', function (req,res)
+{
+  // Do I have an ENV with that name?
+  var envoutput = process.env["CEBROKERURI"];
+
+  if( envoutput == null )
+  {
+    res.send("http://broker-ingress.knative-eventing.svc.cluster.local/{TARGET_NAMESPACE}/default")
+  }
+  else
+  {
+    res.send(envoutput);
+  }
+});
+
+app.listen(port, ip);
+console.log('Server running on ' + ip + ':' + port);
+
